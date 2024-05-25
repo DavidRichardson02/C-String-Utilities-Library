8237f950a7771436a30835eb683e63bd5286643f
David Richardson <02richardsondavid@gmail.com>
2024-05-25 13:59:43 -0400

    le epic time

--- C-String Utilities Library/main.c
+++ C-String Utilities Library/main.c
@@ -1,13 +1,11 @@
-//
 //  main.c
 //  C-String Utilities Library
-//
-//  Created by David Richardson on 5/25/24.
-//
+
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) 
+{
 	// insert code here...
 	printf("Hello, World!\n");
 	return 0;
