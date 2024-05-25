ff0578d67eadd89f71533f9bf009f3cb6f9271a0
David Richardson <02richardsondavid@gmail.com>
2024-05-25 13:43:14 -0400

    otto

--- C-String Utilities Library/AuxiliaryUtilities.h
+++ C-String Utilities Library/AuxiliaryUtilities.h
@@ -1,13 +1,133 @@
-//
 //  AuxiliaryUtilities.h
 //  C-String Utilities Library
-//
-//  Created by David Richardson on 5/25/24.
-//
+//  DavidRichardson02
+/**
+ * AuxiliaryUtilities code: Provides a collection of general utility functions that support various computational
+ * and data manipulation tasks across various contexts. Encompasses functions for safe memory allocation for different data types, including basic and derived types,
+ * and functions for handling date/time conversions, particularly focusing on Unix time conversions.
+ *
+ * Additionally, it includes utilities for bitwise operations on numerical data, useful in scenarios requiring
+ * direct manipulation of the binary representation of data. It also provides robust sorting algorithms, including
+ * merge sort and radix sort, optimized for handling large datasets, especially useful for sorting double precision
+ * floating-point numbers.
+ */
 
 #ifndef AuxiliaryUtilities_h
 #define AuxiliaryUtilities_h
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+
+
+
+
+// ------------------ Global Constants and Macros -------------------
+// \{
+// Macro to compute the number of elements in a statically-allocated array
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+// Preprocessor macro that creates a string representation of its argument, the name of the variable
+#define VAR_NAME_AS_STRING(var) #var
+
+
+static const size_t MAX_STRING_SIZE = 1000; // Maximum string size.
+
+
+/**
+ * 'months' array: This array contains the names of the twelve months of the year.
+ * It is useful for converting numeric month representations into their corresponding
+ * textual names.
+ */
+extern const char *months[12];
+
+/**
+ * 'weekDays' array: This array holds the names of the seven days of the week.
+ * It can be used to map numeric day representations to
+ * their corresponding day names.
+ */
+extern const char *weekDays[7];
+
+/**
+ * 'commonDateTimeFormats' array: An array of string literals representing commonly used date/time formats.
+ * These formats are crucial in parsing and standardizing date/time strings from diverse data sources,
+ * and follow the conventions used in 'strftime' and 'strptime' functions for date-time formatting and parsing.
+ * Each format specifier adheres to the C standard library conventions for date/time formatting.
+ */
+extern const char *commonDateTimeFormats[12]; // Used for converting date/time strings from datasets into standardized formats.
+
+
+static pthread_mutex_t localtime_mutex = PTHREAD_MUTEX_INITIALIZER; //Mutex for Thread Safety protecting access to localtime
+// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Performing Various Mathematical Operations on Containers -------------
+/// \{
+double min(double a, double b); // Returns the minimum of two values.
+double max(double a, double b);  // Returns the maximum of two values.
+
+double min_element(double *data, int n); // Returns the minimum element in an array of values.
+double max_element(double *data, int n); // Returns the maximum element in an array of values.
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Operations with Time -------------
+/// \{
+time_t convert_to_unix_time(const char *dateTimeString); // Converts a date/time string into Unix time.
+struct tm *thread_safe_localtime(const time_t *tim, struct tm *result); // A thread-safe wrapper around localtime.
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Bitwise Operations(on Binary Numerals?) -------------
+/// \{
+uint64_t flip_sign_bit(uint64_t value); // Helper function to flip the sign bit of the double's binary representation.
+uint64_t double_to_uint64(double value); // Helper function to reinterpret a double as an uint64_t.
+double uint64_to_double(uint64_t value); // Helper function to reinterpret a uint64_t as an double.
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Sorting -------------
+/// \{
+void merge_data(double *unsortedData, int left, int middle, int right); // Merges two sorted subarrays into a single sorted array.
+void merge_sort_data(double *unsortedData, int left, int right); // Recursively divides and sorts an array using the merge sort algorithm.
+void merge_sort(double *unsortedData, const int numElements); // Sorts an array of doubles using the merge sort algorithm.
+
+
+
+void radix_sort_doubles(double *unsortedData, const int numElements); // Sorts an array of double precision floating-point numbers by interpreting their bit patterns as unsigned 64-bit integers using Radix Sort.
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Performing Memory Operations -------------
+/// \{
+void *set_memory_block(void *block, int c, size_t n); // Sets the first 'n' bytes of the memory 'block' to the value specified by 'c'
+void *copy_memory_block(void *destination, const void *source, size_t n); // Copies the specified number of bytes from the source memory block to the destination memory block.
+/// \}
+
+
+
+
+
 
 #endif /* AuxiliaryUtilities_h */
--- C-String Utilities Library/StringUtilities.h
+++ C-String Utilities Library/StringUtilities.h
@@ -1,13 +1,145 @@
-//
 //  StringUtilities.h
 //  C-String Utilities Library
-//
-//  Created by David Richardson on 5/25/24.
-//
+//  DavidRichardson02
+/**
+ * StringUtilities code: provides a comprehensive suite of utilities for string manipulation and analysis, primarily designed for data
+ * parsing and preprocessing in the context of data integration and analysis. This collection of functions is particularly useful
+ * in contexts where string data must be examined, transformed, or formatted before being used in computations or visualizations.
+ *
+ * NOTE: Focuses on C-style strings, namely: char, char*, char*[], and char**
+ **/
+
 
 #ifndef StringUtilities_h
 #define StringUtilities_h
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <stdbool.h>
+#include "AuxiliaryUtilities.h"
+
+
+
+
+// ------------- Helper Functions for Determining Properties of Strings in c, namely: char c, char* characterString, char** stringArray -------------
+/// \{
+bool char_is_alpha(char c); // Checks if a character is an alphabetic character.
+bool char_is_digit(char c); // Checks if a character is a digit.
+bool char_is_alnum(char c); // Checks if a character is alphanumeric.
+bool char_is_whitespace(char c); // Checks if a character is whitespace.
+bool char_is_punctuation(char c); // Checks if a character is punctuation.
+bool char_is_underscore(char c); // Checks if a character is an underscore.
+bool char_is_sign(char c); // Checks if a character is a sign (+, -, etc.).
+bool char_is_delimiter(char c); // Checks if a character is a delimiter (non-alphanumeric and non-space)
+
+
+bool string_is_numeric(const char *characterString); // Checks if a string represents a numeric value.
+bool string_is_hyphen_else_is_minus_sign(char *characterString); // Differentiates between hyphens and minus signs.
+int *string_is_date_time(const char *characterString, const char *delimiter, const int fieldCount); // Analyzes a string for date/time formats.
+bool string_array_contains_date_time(char **stringArray, int stringCount, const char *delimiter);
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Counting Elements and Identifying Characteristics of Strings -------------
+/// \{
+int count_character_occurrences(const char *characterString, char c); // Counts occurrences of a character in a string
+char find_most_common_non_alphanumeric_character(const char *characterString); // Finds the most common non-alphanumeric character in a string
+char *find_potential_delimiters(const char *characterString, int *delimiterCount); // Finds potential delimiters in a string
+char *identify_delimiter(char** stringArray, int stringCount); // Identify the most common delimiter in a string
+char *identify_delimiter_recursive(char** stringArray, int stringCount, char **primaryDelimiters, int *primaryDelimiterCount); // Pass in primary delimiters(and count) by reference... a special case function
+const char *determine_string_representation_type(const char* token); // Determines if a string is numeric or non-numeric.
+
+
+size_t character_string_length(const char *characterString); // Returns the length of a character string.
+int count_array_strings(char **stringArray); // Counts strings in an array.
+/// \}
+
+
+
+
+
+
+char *tokenize_character_string(char *s, const char *delim); // Tokenizes a character string based on a delimiter.
+char *duplicate_character_string(const char *characterString);
+char *copy_character_string(char *destination, const char *source); // Copies a character string.
+char *copy_n_character_string(char *destination, const char *source, size_t n); // Copies up to n characters from the string pointed to by 'source' and to 'destination', where any n greater than the length of 'source' is padded with null characters.
+char *concatenate_character_string(char *destination, const char *source); // Concatenates two character strings.
+char *concatenate_n_character_string(char *destination, const char *source, size_t n); // Concatenates up to n characters from the string pointed to by 'source' to the end of the string pointed to by 'destination'.
+
+
+
+
+
+
+// ------------- Helper Functions for Comparing Strings -------------
+/// \{
+char *determine_most_common_string(char **stringArray, int stringCount); // Determines the most common string in an array of strings.
+int compare_character_strings(const char *characterString1, const char *characterString2); // Compares two character strings for equality.
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Combining and/or Concatenating Representations of strings in plain c -------------
+/// \{
+char **split_string_tokens(const char* characterString, const char* delimiter, int divisions); // Splits a string into an array of strings based on a given delimiter,i.e., assigns each tokenized field to an element in an array.
+char *combine_char_ptr(const char *characterString1, const char *characterString2);  // Combines two strings.
+char **combine_char_ptr_ptr(const char **stringArray1, int stringCountArray1, const char **stringArray2, int stringCountArray2); // Combines two arrays of strings.
+char *combine_char_ptr_arr(const char *characterString1, const char *characterStringArray[], int numArrayStrings); // Combines a string with each string in an array.
+char *concatenate_string_array(const char** stringArray, int stringCount, const char *delimiter); // Concatenates an array of strings into a single string with fields seperated by the delimiter
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Trimming and Pruning Strings -------------
+/// \{
+char *replace_date_time_with_unix(char* characterString, const char *delimiter, const int fieldCount); // Replaces date/time with Unix time.
+
+char *trim_string_whitespaces(char* untrimmedString); // Trims whitespace from a string.
+char *prune_string_whitespaces(char *unprunedString); // Removes all whitespaces from a string.
+char *prune_repeated_delimiters_from_string(char *unprunedString, const char *delimiter);  // Handles repeated delimiters.
+char *prune_and_trim_problematic_characters_from_string(char *originalString, const char *delimiter, const int fieldCount);  // Prunes and trims problematic characters.
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Formatting Strings -------------
+/// \{
+
+/// \}
+
+
+
+
+
+
+// ------------- Helper Functions for Printing Various Representations of String and Arrays for Debugging -------------
+/// \{
+void print_string(char* string); // Prints a single string.
+void print_string_array(char** stringArray, int stringCount, char* label); // Prints an array of strings.
+void print_string_array_array(char*** stringArrayArray, int stringArraysCount, int stringSubArraysCount, char* label); // NOTE: assumes the dimension of all sub-arrays is the same
+void print_array(int n, double *data, char* label); // Prints an array of doubles.
+void print_array_array(double **data, int rows, int columns, char* label); // Prints a 2D array of doubles.
+void print_char_ptr_array(char *charPtrArr[], int stringCount, char* label); // Prints an array of char pointers.
+/// \}
+
+
+
+
+
 
 #endif /* StringUtilities_h */
