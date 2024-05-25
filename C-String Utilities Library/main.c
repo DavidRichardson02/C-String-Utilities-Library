a19660d86fdc75061452c4486768b48fd979fdc3
David Richardson <02richardsondavid@gmail.com>
2024-05-25 07:04:09 -0400

    Initial Commit

---
+++ C-String Utilities Library/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-String Utilities Library
+//
+//  Created by David Richardson on 5/25/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
